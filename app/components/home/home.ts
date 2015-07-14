0cb77c531da1e73b991b6771f170de893600bdcc
Robert Gardner <rgardner@google.com>
2015-07-13 21:54:03 -0700

    Move chart into its own component.
    Create migration-map component to hold the map.

--- app/components/home/home.ts
+++ app/components/home/home.ts
@@ -1,13 +1,14 @@
 import {Component, View, NgFor} from 'angular2/angular2';
 
 import {NamesList} from '../../services/NameList';
+import {MigrationMap} from '../../components/migration-map/migration-map';
 
 @Component({
   selector: 'component-1'
 })
 @View({
   templateUrl: './components/home/home.html?v=<%= VERSION %>',
-  directives: [NgFor]
+  directives: [MigrationMap, NgFor]
 })
 export class Home {
   constructor(public list: NamesList) {
---
+++ app/components/migration-map/migration-map.ts
@@ -0,0 +1,24 @@
+/// <reference path="../../../typings/tsd.d.ts" />
+import {Component, View} from 'angular2/angular2';
+
+@Component({
+  selector: 'migration-map'
+})
+@View({
+  templateUrl: './components/migration-map/migration-map.html?v=<%= VERSION %>',
+  directives: []
+})
+export class MigrationMap {
+  place: string;
+  decade: string;
+  direction: string;
+
+  constructor() {
+  }
+
+  draw(place: string, decade: string, direction: string) {
+    this.place = place;
+    this.decade = decade;
+    this.direction = direction;
+  }
+}
