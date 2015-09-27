f1bb7ce9cc092c195d8eb87ba262284f6012cc9f
Robert Gardner <rgardner@google.com>
2015-09-26 22:03:58 -0700

    Get the chart data via HTTP
    Uses HTTP to retrieve the chart data as JSON.
    
    Haven’t yet figured out how to get the server to actually return JSON.
    For now, it just returns an HTML text file with the JSON text.

--- app/components/migration-map/migration-map.ts
+++ app/components/migration-map/migration-map.ts
@@ -18,7 +18,6 @@ export class MigrationMap {
   options: Object;
 
   http: Http;
-  response: string;
 
   constructor(http: Http) {
     this.type = "pie";
@@ -30,31 +29,22 @@ export class MigrationMap {
   }
 
   refresh() {
+    this.http.get('stuff.html')
+      //Get the RxJS Subject
+      .toRx()
+      // Call map on the response observable to get the parsed data
+      .map(res => res.json())
+      // Subscribe to the observable to get the parsed data and attach it to the component
+      .subscribe(h => this.update(h));
+  }
+
+  update(data: Ojbect[]) {
     this.type = "geo";
     this.options = {
       "region": "IT",
       "displayMode": "markers",
       "colorAxis": { "colors": ["green", "blue"] }
     };
-    this.data = [
-      ["City", "Population", "Density"],
-      ["Rome", 3456789, 1285.31],
-      ["Milan", 1324110, 181.76],
-      ["Naples", 959574, 117.27],
-      ["Turin", 907563, 130.17],
-      ["Palermo", 655875, 158.9],
-      ["Genoa", 607906, 243.60],
-      ["Bologna", 380181, 140.7],
-      ["Florence", 371282, 102.41],
-      ["Fiumicino", 67370, 213.44],
-      ["Anzio", 52192, 43.43],
-      ["Ciampino", 38262, 11]
-    ];
-    this.http.get('stuff.html')
-      //Get the RxJS Subject
-      .toRx()
-      // Subscribe to the observable to get the parsed people object and attach it to the
-      // component
-      .subscribe(h => this.response = h.text());
+    this.data = data;   
   }
 }
